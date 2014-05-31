3908e3b133d4e1968f59ccfbbaf26b4d88544978
sanjaysingh13 <sanjaysingh13@gmail.com>
2014-05-31 15:17:42 +0530

    carousel done. trying for index page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -27,4 +27,16 @@ class Contact < ActiveRecord::Base
     ws.save
   end
 
+  def subscribe
+    mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
+    result = mailchimp.lists.subscribe({
+      :id => Rails.application.secrets.mailchimp_list_id,
+      :email => {:email => self.email},
+      :double_optin => false,
+      :update_existing => true,
+      :send_welcome => true
+    })
+    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+  end
+
 end
