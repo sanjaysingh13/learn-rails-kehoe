a6c434bb60e78b26cfec14ec0e610a8d2c17cd2f
sanjaysingh13 <sanjaysingh13@gmail.com>
2014-06-02 22:40:42 +0530

    datatypes

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,2 +1,9 @@
 class Film < ActiveRecord::Base
+  column :id, :serial
+  column :name, :char(100)
+  column :link, :varchar(1000)
+  column :url, :string(50)
+  column :writeup, :varchar(1000)
+  column :credits, :varchar(1000)
+  column :still1, :string(10)
 end
