973deec911a544614773e40a4069fadd355f3c34
sanjaysingh13 <sanjaysingh13@gmail.com>
2014-06-02 23:00:07 +0530

    datatypesgit add -A

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,9 +1,3 @@
 class Film < ActiveRecord::Base
-  column :id, :primary_key
-  column :name, :string(100)
-  column :link, :text(1000)
-  column :url, :string(50)
-  column :writeup, :text(1000)
-  column :credits, :text(1000)
-  column :still1, :string(10)
+  
 end
